6d6492105eaa460dadb6dc01cefee037436d2c5d
owner <owner@DESKTOP-4D19PQA>
2024-10-14 20:37:44 +0800

    加入專案檔案。

---
+++ nxn乘法表/nxn乘法表.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void printMultiplicationTable(int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            std::cout << i * j << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "請輸入一個數字: ";
+    std::cin >> n;
+    printMultiplicationTable(n);
+    return 0;
+}
